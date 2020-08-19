40cb6d8ea877fbef5e83d3fb78e5a20827717c1b
paranjay <paranjay.bind@gmail.com>
2020-08-20 01:32:59 +0530

    Alex updated README and added his first files to his folder.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-js-learn
+### Learning JavaScript with Sanika and Alex
+
+Alex and Sanika are both learning JavaScript and this repository documents and contains the code they generate while learning this awesome script.
+
+## Different Directories
+
+To avoid confusion, they both have different dedicated folders for themselves.
+    
+    **Sanika: /kanojo**
+    **Alex  : /kareshi**
+
+## Bugs and Errors
+
+They're both learning so some bugs and errors are expected. If you see any, please raise issues about the respective bugs and errors.
+
+**__You learn, only to forget. Sometimmes the path may matter mmore than the destination.__**
---
+++ kareshi/console_intro.js
@@ -0,0 +1,30 @@
+// logging to console
+
+//string
+console.log('Hello Baka World!');
+//number
+console.log(666);
+//boolean
+console.log(false);
+//variable
+var kanojo = "Sanika";
+console.log(kanojo);
+//array
+console.log(['s','a','n','i','k','a']);
+//object
+console.log({san:'sweetie', ika:'pie'});
+//table
+console.table({san:'sweetie', ika:'pie'});
+//error
+console.error('Sanika has no real errors.');
+//warning
+console.warn('All men be warned, staying away from girl is healthier than going against me.');
+//time taken by a process or funtion
+console.time('same_id');
+    console.log(kanojo);
+    console.log(kanojo);
+    console.log(kanojo);
+    console.log(kanojo);
+console.timeEnd('same_id');
+//clearing the console
+// console.clear();
