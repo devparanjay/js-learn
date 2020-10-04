07e428a74fce3181546b0b61b7a81642a42cb94a
Sanika19 <sanugr8@gmail.com>
2020-10-04 19:08:10 +0530

    First again. XD

--- kanojo/app.js
+++ kanojo/app.js
@@ -11,11 +11,11 @@ console.log({San:1,Alex:8});
 console.table({San:'kanojo',Alex:'kareshi'});
 console.error("Hello, this is a fake error.");
 console.warn("Hello, this is a fake warning.");
-console.time("Start");
+console.time("Here");
 console.log("Alex is silly.");
 console.log("Alex is silly.");
 console.log("Alex is silly.");
 console.log("Alex is silly.");
 console.log("Alex is silly.");
-console.timeEnd("End");
+console.timeEnd("Here");
 //This is it
