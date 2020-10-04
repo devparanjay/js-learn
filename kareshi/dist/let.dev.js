34ce4f0d3a6bfd7f082453c69d70ee3478a7af8f
paranjay <paranjay.bind@gmail.com>
2020-10-04 20:13:32 +0530

    Usage of var, let, and const. Updated index.html.

---
+++ kareshi/dist/let.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Using let.
+
+/*
+    *****
+    *let*
+    *****
+*/
+// Using let and logging it.
+var Kanojo = "Sanika";
+console.log(Kanojo); // Changing let.
+
+Kanojo = "Sweetheart";
+console.log(Kanojo); // Initiating let.
+
+var Sweetheart;
---
+++ kareshi/var.js
@@ -0,0 +1,37 @@
+// Using var.
+
+/*
+    *****
+    *var*
+    *****
+*/
+
+// Storing a var and logging it.
+var kanojo = "Sanika";
+console.log(kanojo);
+
+// Changing the var.
+kanojo = "Sweetheart";
+console.log(kanojo)
+
+// Initiation the var.
+var sweetheart;
+
+// var is undefined.
+console.log(sweetheart);
+
+// Defining the var.
+sweetheart = kanojo;
+console.log(kanojo);
+
+
+// Naming Conventions
+
+// Camel Case
+var sweetiePie = kanojo;
+
+// Underscore
+var sweetie_pie = kanojo;
+
+// Pascal Case
+var SweetiePie = kanojo;
