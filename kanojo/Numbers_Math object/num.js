bd616cbd62a71380b7851841fe30c471c5a92955
Sanika19 <sanugr8@gmail.com>
2020-10-18 15:08:24 +0530

    Numbers and the Math object

---
+++ kanojo/Numbers_Math object/num.js
@@ -0,0 +1,26 @@
+const num1 = 50;
+const num2 = 80;
+let val;
+
+val = num1 + num2;
+val=num1*num2;
+val=num1-num2;
+val=num1/num2;
+val=num1%num2;
+
+//Math object
+val=Math.PI;
+val=Math.E;
+val=Math.round(2.8);
+val=Math.ceil(2.3);
+val=Math.floor(2.9);
+val=Math.sqrt(4);
+val=Math.abs(-8);
+val=Math.pow(2,3);
+val=Math.min(25,46,7,9,90);
+val=Math.max(25,46,7,9,90);
+val=Math.random();
+val=Math.random()*20+1; //random numbers from 1-20
+val=Math.floor(Math.random()*20+1);
+
+console.log(val);
